7032ecceaac38f2f13c1dfcba799ddeded1aabe4
giorgiocost <gegiorgio1991@gmail.com>
2022-01-09 02:34:55 -0300

    class stack

---
+++ js/pilhas.js
@@ -0,0 +1,31 @@
+function Stack() {
+    var items = [];
+
+    this.push = function(element) {
+        // adicionar um novo item a pilha
+    }
+
+    this.pop = function() {
+        // remover o item do topo da pilha
+    }
+
+    this.peek = function() {
+        // devolve o elemento que está no topo da pilha
+    }
+
+    this.isEmpty = function() {
+        // informa se a pilha está vazia
+    }
+
+    this.clear = function() {
+        // limpa a pilha
+    }
+
+    this.size = function() {
+        // informa o tamanho da pilha
+    }
+
+    this.print = function() {
+        // imprime a pilha no console
+    }
+}
