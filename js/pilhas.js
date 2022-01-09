7f6947314e04f56ae9b88cfa666881ff56e1431a
giorgiocost <gegiorgio1991@gmail.com>
2022-01-09 02:50:17 -0300

    Implementação inicial

--- js/pilhas.js
+++ js/pilhas.js
@@ -3,29 +3,36 @@ function Stack() {
 
     this.push = function(element) {
         // adicionar um novo item a pilha
+        items.push(element);
     }
 
     this.pop = function() {
         // remover o item do topo da pilha
+        return items.pop();
     }
 
     this.peek = function() {
         // devolve o elemento que está no topo da pilha
+        return items[items.length - 1];
     }
 
     this.isEmpty = function() {
         // informa se a pilha está vazia
+        return !Object.values(items).length;
     }
 
     this.clear = function() {
         // limpa a pilha
+        items = [];
     }
 
     this.size = function() {
         // informa o tamanho da pilha
+        return items.length;
     }
 
     this.print = function() {
         // imprime a pilha no console
+        console.log(items.toString());
     }
 }
