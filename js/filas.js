3f9c3d002a401eab656b610fb8df7f060d666173
giorgiocost <gegiorgio1991@gmail.com>
2022-01-09 13:54:39 -0300

    Implementação inicial

--- js/filas.js
+++ js/filas.js
@@ -1,27 +1,33 @@
 function Queue() {
     var items = [];
 
-    this.enqueue = function() {
+    this.enqueue = function(element) {
         // adicionar um novo item
+        items.push(element);
     }
 
     this.dequeue = function() {
         // remove um item
+        return items.shift();
     }
 
     this.front = function() {
         // retorna o primeiro elemento da fila
+        return items[0];
     }
 
     this.isEmpty = function() {
         // verifica se a fila está vazia
+        return !Object.values(items).length;
     }
 
     this.size = function() {
         // retorna o tamanho da fila
+        return items.length;
     }
 
     this.print = function() {
         // imprime a fila no console
+        console.log(items.toString());
     }
 }
