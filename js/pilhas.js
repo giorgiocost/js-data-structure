e0800479abcc564ddc9a5a2066705ac770f24a03
giorgiocost <gegiorgio1991@gmail.com>
2022-01-09 04:30:25 -0300

    Exercício conversão de base pt1

--- js/pilhas.js
+++ js/pilhas.js
@@ -73,3 +73,22 @@ function dec2Bin(decNumber) {
 }
 
 console.log(dec2Bin(25));
+
+function baseConverter(decNumber, base) {
+    var restStack = [],
+    rest,
+    baseString = '',
+    digits = '0123456789ABCDEF';
+
+    while(decNumber > 0) {
+        rest = Math.floor(decNumber % base);
+        restStack.push(rest);
+        decNumber = Math.floor(decNumber / base);
+    }
+
+    while(restStack.length > 0) {
+        baseString += digits[restStack.pop()];
+    }
+
+    return baseString;
+}
