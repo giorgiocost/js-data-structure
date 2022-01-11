bc1d3f759ef487fefca341f1078956ec20377b69
giorgiocost <gegiorgio1991@gmail.com>
2022-01-10 22:53:53 -0300

    Fila de prioridades pt1

--- js/filas.js
+++ js/filas.js
@@ -46,3 +46,45 @@ fila.dequeue();
 fila.print();
 console.log(fila.isEmpty());
 console.log(fila.size());
+
+function PriorityQueue() {
+    var items = [];
+
+    function QueueElement(element, priority) {
+        this.element = element;
+        this.priority = priority;
+    }
+
+    this.enqueue = function(element, priority) {
+        var queueElement = new QueueElement(element, priority);
+        var added = false;
+
+        for(var i = 0; i < items.length; i++) {
+            if(queueElement.priority < items[i].priority) {
+                items.splice(i, 0, queueElement);
+                added = true;
+                break;
+            }
+        }
+        if(!added) {
+            items.push(queueElement);
+        }
+    }
+
+    this.dequeue = function() {
+        return items.shift();
+    }
+
+    this.print = function() {
+        for( var i = 0; i < items.length; i++) {
+            console.log(`${items[i].element} ${items[i].priority}`);
+        }
+    }
+}
+
+var pqueue = new PriorityQueue();
+
+pqueue.enqueue('Laranja', 2);
+pqueue.enqueue('Banana', 1);
+pqueue.enqueue('Limão', 1);
+pqueue.print();
