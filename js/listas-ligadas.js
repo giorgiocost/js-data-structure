453ccf07bc0103cdb38c5a4e591d11220da112fc
giorgiocost <gegiorgio1991@gmail.com>
2022-01-15 20:51:45 -0300

    Introdução às Listas ligadas

---
+++ js/listas-ligadas.js
@@ -0,0 +1,46 @@
+function LinkedList() {
+
+    var Node = function(element) {
+        this.element = element;
+        this.next = null;
+    }
+
+    var length = 0;
+    var head = null;
+
+    this.append = function(element) {
+        // adiciona um elemento no final da lista
+    }
+
+    this.inseri = function() {
+        // adiciona um elemento em uma posição específica
+    }
+
+    this.removeAt = function(position) {
+        // remove o elemento de uma posição específica
+    }
+
+    this.remove = function(element) {
+        // remove o elemento element
+    }
+
+    this.indexOf = function(element) {
+        // retorna a posição do elemento
+    }
+
+    this.isEmpty = function() {
+        // retorna se está vazio
+    }
+
+    this.size = function() {
+        // retorna o tamanho da instancia
+    }
+
+    this.toString = function() {
+        // converte para string
+    }
+
+    this.print = function() {
+        // imprime no console
+    }
+}
